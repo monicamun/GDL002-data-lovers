2f00a729657d0857c75c0f93984a57d1c920aaba
monicamun <mymunoznava@gmail.com>
2019-02-20 17:35:01 -0600

    Funciones de ocultar y mostrar pantallas, funcion ordenar alfabeticamente

--- src/main.js
+++ src/main.js
@@ -7,26 +7,53 @@ let cardTemplate = `<div class="card">
  <h3>{pokemon.number}</h3>
 </div>
 <div class="type">
- <h3>{pokemon.type}</h3>
+<h3>{pokemon.type}</h3>
 </div>
 </div>`;
 
-let listHtml = dataLovers.getAllPokemon().map(pokemon => {
+let content = document.getElementById("content");
+
+function displayPokemonCards(pokemonArray) {
+  let listHtml = pokemonArray.map(pokemon => fillCardTemplate(pokemon));
+  let htmlString = listHtml.join(" ");
+  content.innerHTML = htmlString;
+}
+
+function fillCardTemplate(pokemon) {
   return cardTemplate
     .replace("{pokemon.img}", pokemon.img)
     .replace("{pokemon.name}", pokemon.name)
-    .replace("{pokemon.name}",pokemon.name)
-    .replace("{pokemon.number}",pokemon.num)
-    .replace("{pokemon.type}",pokemon.type);
-});
+    .replace("{pokemon.name}", pokemon.name)
+    .replace("{pokemon.number}", pokemon.num)
+    .replace("{pokemon.type}", pokemon.type);
+}
 
+function alphabeticalOrder() {
+  let sortedPokemon = dataLovers.getAllPokemon.sort(function(prev, next) {
+    if (prev.name > next.name) {
+      return 1;
+    }
+    if (prev.name < next.name) {
+      return -1;
+    }
+    return 0;
+  });
 
+  displayPokemonCards(sortedPokemon);
+}
 
+document.getElementById("btn1").addEventListener("click", mostrar);
+document.getElementById("btn2").addEventListener("click", ocultar);
 
-let htmlString = listHtml.join(' ');
+function mostrar() {
+  document.getElementById("content").style.display = "block";
+  document.getElementById("Ocultar").style.display = "none";
+}
 
-let content = document.getElementById('content');
+function ocultar() {
+  document.getElementById("content").style.display = "none";
+  document.getElementById("Ocultar").style.display = "block";
+}
 
-content.innerHTML = htmlString;
-
-
+//llenar con lista default de pokemon
+displayPokemonCards(dataLovers.getAllPokemon());
