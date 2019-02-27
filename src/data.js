29edf2a729b43be390ed604aa0600a8e1be6067e
monicamun <mymunoznava@gmail.com>
2019-02-26 18:28:37 -0600

    creacion de banner

--- src/data.js
+++ src/data.js
@@ -10,5 +10,11 @@ window.example = example;
 window.dataLovers = {
   getAllPokemon: () => {
     return pokemonDb.array;
+  },
+  getPokemonByNum: num => {
+    return pokemonDb.array.find(p => p.num === num);
+  },
+  getPokemonByTypeFilter: type => {
+    return pokemonDb.array.filter(p => p.type.indexOf(type) > -1);
   }
 };
--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+// var declarations
+let sliderInterval;
+
 let cardTemplate = `<div class="col-3">
 <div class="card " id="{pokemon.number}" >
 <img class="pkm-thmb" src="{pokemon.img}" alt="{pokemon.name}">
@@ -13,13 +16,28 @@ let cardTemplate = `<div class="col-3">
 </div>
 </div>`;
 
-let pokemonInfoTemplate = `
+let pokemonInfoTemplate = `<div class="col-12">
+<div class="card " id="{pokemon.number}" >
+<img class="pkm-thmb" src="{pokemon.img}" alt="{pokemon.name}">
+<div class="name">
+ <h3>{pokemon.name}</h3>
+</div>
+<div class="number">
+ <h3>{pokemon.number}</h3>
+</div>
+<div class="type">
+<h3>{pokemon.type}</h3>
+</div>
+</div>
+</div>
 `;
 
 document.getElementById("btn1").addEventListener("click", mostrar);
 document.getElementById("btn2").addEventListener("click", ocultar);
-document.getElementById("orderABC").addEventListener("click", alphabeticalOrder);
-document.getElementById("orderNum").addEventListener("click",numericalOrder );
+document
+  .getElementById("orderABC")
+  .addEventListener("click", alphabeticalOrder);
+document.getElementById("orderNum").addEventListener("click", numericalOrder);
 
 let content = document.getElementById("content");
 
@@ -28,8 +46,14 @@ function displayPokemonCards(pokemonArray) {
   let htmlString = listHtml.join(" ");
   content.innerHTML = htmlString;
 
-  Array.from( document.getElementsByClassName("card") ).forEach(element => {
-    element.addEventListener("click", () => console.log(element.id))
+  Array.from(document.getElementsByClassName("card")).forEach(element => {
+    element.addEventListener("click", () => {
+      let selectedPokemon = dataLovers.getPokemonByNum(element.id);
+      content.innerHTML = pokemonInfoTemplate.replace(
+        "{pokemon.img}",
+        selectedPokemon.img
+      );
+    });
   });
 }
 
@@ -58,7 +82,6 @@ function alphabeticalOrder() {
 }
 
 function numericalOrder() {
-
   let numPokemon = dataLovers.getAllPokemon().sort(function(prev, next) {
     if (prev.num > next.num) {
       return 1;
@@ -68,18 +91,51 @@ function numericalOrder() {
     }
     return 0;
   });
-  displayPokemonCards(numPokemon); 
+  displayPokemonCards(numPokemon);
 }
 
+// muestra pokedex, oculta principal
 function mostrar() {
+  clearInterval(sliderInterval);
   document.getElementById("pokedex").style.display = "block";
   document.getElementById("ocultar").style.display = "none";
 }
-
+// oculta pokedex, muestra principal
 function ocultar() {
+  startSliderInterval()
   document.getElementById("pokedex").style.display = "none";
   document.getElementById("ocultar").style.display = "block";
 }
 
 //llenar con lista default de pokemon
 displayPokemonCards(dataLovers.getAllPokemon());
+
+let imageIndex = 0;
+function banner() {
+  
+  let images = [
+    "imagenes pokemon/banner_4.jpg",
+    "imagenes pokemon/banner_2.jpg",
+    "imagenes pokemon/banner_6.jpg"
+  ];
+
+  document.banner.src = images[imageIndex];
+
+  if (imageIndex < images.length - 1) {
+    imageIndex++;
+  } else {
+    imageIndex = 0;
+  }
+}
+
+function startSliderInterval() {
+  // espera 5 segundos
+  sliderInterval = setInterval(banner, 5000)
+}
+
+// empieza el intervalo al cargar el js
+startSliderInterval();
+// carga el primer banner sin esperar los 5 segundos
+banner();
+
+
